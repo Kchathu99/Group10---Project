0586ddccd9ca0862907ab73d831976dae667e307
Kchathu99 <121050529+Kchathu99@users.noreply.github.com>
2023-07-15 11:17:34 +0530

    Add files via upload

--- src/Nav.js
+++ src/Nav.js
@@ -1,26 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './nav.css';
 import logoImage from './images/icon_2.png';
-
 
 const Nav = () => {
   return (
     <div>
-        <nav>
+      <nav>
         <ul className="navbar">
-            <li className="navbar__item"><Link to="/">Home</Link></li>
+          <li className="navbar__item"><Link to="/">Home</Link></li>
           <li className="navbar__item"><Link to="/menu">Menu</Link></li>
           <li className="navbar__item"><Link to="/drinks">Drinks</Link></li>
-         <li className="navbar__item"><Link to="/deserts">Reservation</Link></li>
+          <li className="navbar__item"><Link to="/deserts">Reservation</Link></li>
           <li className="navbar__item"><Link to="/login">Login</Link></li>
           <li className="navbar__item"><Link to="/profile">Profile</Link></li>
-  <li className="navbar__item"><Link to="/logout"><img src={logoImage} alt="Logo" className="navbar__logo" /></Link></li>
+          <li className="navbar__item">
+            <Link to="/logout">
+              <img src={logoImage} alt="Logo" className="navbar__logo" />
+            </Link>
+          </li>
         </ul>
       </nav>
-
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
